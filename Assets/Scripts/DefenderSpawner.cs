2349e7020f7466fd32878c2130134d25a5a4c4ff
techguyivan <ibivanburgos@gmail.com>
2021-03-11 23:35:25 -0800

    Adding Glitch Garden through Github Desktop

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour {
+
+    Defender defender;
+
+    private void OnMouseDown()
+    {
+        SpawnDefender(GetSquareClicked());
+    }
+
+    public void SetSelectedDefender(Defender defenderToSelect)
+    {
+        defender = defenderToSelect;
+    }
+
+    private Vector2 GetSquareClicked()
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        Vector2 gridPos = SnapToGrid(worldPos);
+        /*Debug.Log(gridPos);*/
+        return gridPos;
+    }
+
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
+    {
+
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+        /*Debug.Log(rawWorldPos);*/
+        return new Vector2(newX, newY);
+    }
+
+    private void SpawnDefender(Vector2 roundedPos)
+    {
+        Defender newDefender = Instantiate(defender, roundedPos, Quaternion.identity) as Defender;
+        Debug.Log(roundedPos);
+    }
+}
